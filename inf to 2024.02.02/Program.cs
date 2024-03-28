67bafd8bb4bd2218146e1ef496b58e745a21f8df
UPLAPPY <107671246+UPLAPPY@users.noreply.github.com>
2024-03-28 19:57:47 +0300

    deletes stuff

--- inf to 2024.02.02/Program.cs
+++ inf to 2024.02.02/Program.cs
@@ -13,21 +13,7 @@ namespace inf_to_2024._02._02
     {
         public static void Main()
         {
-            IPrinter[] printer = new IPrinter[4];
-            IElGenerator<int> IntGenerator = new GeneratorInt();
-            IElGenerator<string> StringGenerator = new GeneratorString();
-            IElGenerator<double> DoubleGenerator = new GeneratorDouble();
-            IElGenerator<bool> BoolGenerator = new GeneratorBool();
-
-            printer[0] = new Array1<double>(false, DoubleGenerator);
-            printer[1] = new Array1<string>(true, StringGenerator);
-            printer[2] = new Array2<int>(false, IntGenerator);
-            printer[3] = new Array2<bool>(true, BoolGenerator);
-
-            for (int i = 0; i < printer.Length; i++)
-            {
-                printer[i].PrintArray();
-            }
+            
         }
     }
 }
